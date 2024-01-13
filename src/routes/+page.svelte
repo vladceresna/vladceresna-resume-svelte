ba035dafca33968e4ba67f87f822a5b303fdeedc
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-01-13 12:56:42 +0200

    Update pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,100 @@
 <script>
-  
+  import LeftBlock from '../lib/components/LeftBlock.svelte';
+  import RightBlock from '../lib/components/RightBlock.svelte';
+  import {
+    Badge
+  } from '@sveltestrap/sveltestrap';
+
 </script>
 <style>
-  .containerr {
-      display: grid;
-      grid-gap: 5px;
-      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-      grid-template-rows: repeat(0, minmax(300px, 0.33fr));
-  }
-</style>
 
-<div class="containerr">
+</style>
+<div class="page-container">
+
+  <div style=""><RightBlock 
+    title="Java Developer" alt="" 
+    imagelink="">
+    <Badge>Spring</Badge>
+    <Badge>Vaadin</Badge>
+    <Badge>SpringMVC</Badge>
+    <Badge>Rest API</Badge>
+    <Badge>Hibernate</Badge>
+    <Badge>Postgres</Badge>
+    <Badge>Redis/RedisJSON</Badge>
+    <br><br>
+    <h3>Projects</h3>
+    <ul>
+      <li><a href="/projects/#">Rula</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+    
+  </RightBlock></div>
+  <div style=""><LeftBlock 
+    title="Android Developer" alt="" 
+    imagelink="">
+    <Badge>Firebase</Badge>
+    <Badge>Retrofit</Badge>
+    <Badge>Material UI</Badge>
+    <Badge>XML</Badge>
+    <Badge>Maven</Badge>
+    <Badge>Gradle</Badge>
+    <br><br>
+    <h3>Projects</h3>
+    <ul>
+      <li><a href="/projects/#rula-retsystem">Rula Retsystem</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </LeftBlock></div>
+  <div style=""><RightBlock 
+    title="Web Developer" alt="" 
+    imagelink="">
+    <Badge>HTML/CSS</Badge>
+    <Badge>Adaptive Web</Badge>
+    <Badge>Svelte</Badge>
+    <Badge>JS/TS</Badge>
+    <Badge>Thymeleaf</Badge>
+    <Badge>SpringMVC</Badge>
+    <Badge>Vaadin</Badge>
+    <br><br>
+    <h3>Projects</h3>
+    <ul>
+      <li><a href="/projects/#rula-retsystem">Rula Retsystem</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+      
+  </RightBlock></div>
+  <div style=""><LeftBlock 
+    title="ML Developer" alt="" 
+    imagelink="">
+    <Badge>Neuroph Framework</Badge>
+    <Badge>Neuronets</Badge>
+    <br><br>
+    <h3>Projects</h3>
+    <ul>
+      <li><a href="/projects/#">Rula</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+      
+  </LeftBlock></div>
+  <div style=""><RightBlock 
+    title="UI/UX Designer" alt="" 
+    imagelink="">
+    <Badge>Figma</Badge>
+    <Badge>FigJam</Badge>
+    <Badge>Logotypes</Badge>
+    <Badge>UI/UX</Badge>
+    <br><br>
+    <h3>Projects</h3>
+    <ul>
+      <li><a href="/projects/#">Rula</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+      
+  </RightBlock></div>
 
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,25 @@
-<script></script>
+<script>
+  import LeftBlock from '../../lib/components/LeftBlock.svelte';
+  import RightBlock from '../../lib/components/RightBlock.svelte';
+  import {
+    Badge
+  } from '@sveltestrap/sveltestrap';
+
+</script>
+<style>
+
+</style>
+<div class="page-container">
+    <div style="" id="rula-retsystem"><RightBlock 
+        title="Rula Retsystem" alt="" 
+        imagelink="">
+        <Badge>HTML/CSS</Badge>
+        <Badge>Adaptive Web</Badge>
+        <Badge>Lending Page</Badge>
+        <Badge>Android</Badge>
+        <Badge>Firebase</Badge>
+        <Badge></Badge>
+        <Badge>Android</Badge>
+          
+      </RightBlock></div>
+</div>
