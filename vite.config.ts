cfc6d244c4a3caae47a9297d2fdd95420c9cdb89
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-06-30 19:02:28 +0300

    Image Optimize

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { imagetools } from 'vite-imagetools';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'; 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+    plugins: [
+        sveltekit(),
+        imagetools(),
+        ViteImageOptimizer({
+            // webp: {
+            //     quality: 80,
+            // },
+            // jpeg: {
+            //     quality: 75,
+            // },
+            // png: {
+            //     quality: 75,
+            // },
+            // avif: {
+            //     quality: 70,
+            // },
+            cache: true,
+        }),
+    ],
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
 });
